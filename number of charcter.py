c6a9fbcb335d03bd45c23668a9fc929e40700054
ssj88 <93881947+ssj88@users.noreply.github.com>
2021-12-20 12:27:13 +0530

    Delete number of charcter.py

--- number of charcter.py
+++
@@ -1,8 +0,0 @@
-test_str=str(input("Enter the string : "))
-freq = {}
-for i in test_str:
-    if i in freq:
-        freq[i] += 1
-else:
-    freq[i] = 1
-print ("Count of all characters : "+ str(freq))
