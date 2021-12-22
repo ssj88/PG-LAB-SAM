71d0c77bd7748ce6169773d4421c397ceed99b23
ssj88 <93881947+ssj88@users.noreply.github.com>
2021-12-22 15:27:14 +0530

    Add files via upload

---
+++ c03/Math.py
@@ -0,0 +1,14 @@
+import math
+print("The value of pi is",math.pi)
+
+import math as m
+print("The value of pi is",m.pi)
+
+from math import pi,sqrt
+
+print("The value of pi is ",pi)
+print("The square root of 4 is",sqrt(4))
+
+print(math.cos(90))
+print(math.sin(3))
+print(math.tan(0))
---
+++ c03/graphics.py
@@ -0,0 +1,5 @@
+def area(x,y):
+    print(x*y)
+def peri(x,y):
+    print(2(x+y))
+    
